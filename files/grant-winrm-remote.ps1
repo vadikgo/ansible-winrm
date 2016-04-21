1724dce01283f9fa47420a3cd27071407468d6f3
Горбунов Владислав <vvgorbunov.sbt@sberbank.ru>
2016-04-21 12:04:51 +0300

    add script for setup winrm configSDDL default

---
+++ files/grant-winrm-remote.ps1
@@ -0,0 +1,17 @@
+# grant-winrm-remote.ps1 group_name
+
+Param
+(
+    [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+    $SamAccountName
+)
+
+
+$sid=(New-Object Security.Principal.NTAccount $SamAccountName).Translate([Security.Principal.SecurityIdentifier]).Value
+
+$args = @("set",
+          "winrm/config/service",
+          -join("@{RootSDDL=`"O:NSG:BAD:P(A;;GX;;;", $sid, ")(A;;GA;;;BA)S:P(AU;FA;GA;;;WD)(AU;SA;GXGW;;;WD))`"}")
+)
+
+&"C:\Windows\System32\winrm.cmd" $args
