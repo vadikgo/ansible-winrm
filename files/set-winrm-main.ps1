4e205e82ccbac3a8b0be29a4731cc13c8ff86166
Горбунов Владислав <vvgorbunov.sbt@sberbank.ru>
2016-04-21 15:33:28 +0300

    all actions in one scripts

---
+++ files/set-winrm-main.ps1
@@ -0,0 +1,22 @@
+# join all scripts to one command
+
+$groupName = "Remote Management Users"
+
+Push-Location(Split-Path($MyInvocation.MyCommand.Path))
+
+.\create-group.ps1 $groupName
+echo "Local group", $groupname, "added"
+
+.\Add-PoShEndpointAccess.ps1 -SamAccountName $groupName -EndpointName Microsoft.PowerShell
+
+$arc = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+if ( $arc -eq "64-bit") {
+  .\Add-PoShEndpointAccess.ps1 -SamAccountName $groupName -EndpointName Microsoft.PowerShell32
+}
+.\Add-PoShEndpointAccess.ps1 -SamAccountName $groupName -EndpointName Microsoft.PowerShell.Workflow
+Get-Service -Name WinRM | Restart-Service
+.\grant-winrm-remote.ps1 $groupName
+
+.\Set-WmiNamespaceSecurity.ps1 root/cimv2 add $groupName Enable,RemoteAccess
+
+Pop-Location
